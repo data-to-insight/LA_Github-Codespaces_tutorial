57407ca98e591c037f2dc0df0803fe654e6ebc73
willLPD2I <108789355+willLPD2I@users.noreply.github.com>
2022-08-23 09:35:19 +0000

    added users.py and .replit

---
+++ users.py
@@ -0,0 +1,7 @@
+'''This file is used in the Replit/Github tutorial on the D2I website. The aim is to provide a simple task that will get users familiar with how to make changes to code on the D2I Github repos and start collaborating and writing validation rules. The goal of the task is to get this code from the D2I Github into your Replit, add your name and LA to the dictionary below, run the file to check it works, then commit it back to the repo, ready to be merged by someone from D2I.'''
+
+users = {
+  'Will':'Data to Insight'
+}
+
+print(users)
